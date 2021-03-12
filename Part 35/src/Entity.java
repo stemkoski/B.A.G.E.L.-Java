e93165ee21139b391220bb1d983243cad0722c14
Lee Stemkoski <stemkoski@gmail.com>
2021-03-12 16:50:48 -0500

    adding Part 35 code

---
+++ Part 35/src/Entity.java
@@ -0,0 +1,18 @@
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * Base class for all objects or collections of objects
+ * that can be rendered to the screen.
+ */
+public abstract class Entity
+{
+    /**
+     * Render this Entity to a canvas. 
+     * @param context GraphicsContext object that handles drawing to the canvas
+     */
+    public abstract void draw(GraphicsContext context);
+
+    public abstract void update(double dt);
+
+}
